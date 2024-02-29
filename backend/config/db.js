7a4aaa6678b0962139b97ee5f5154258de9e8064
Tarun Ajjarapu <>
2024-02-29 11:58:25 -0600

    Added mongodb image with a named volume for db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config')
 require('dotenv').config()
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${ conn.connection.host }`)
+        const conn = await mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+        console.log(`MongoDB Connected:`)
     } catch (error) {
-        process.exit(1)
+        console.log(error)
+        setTimeout(connectDB, 5000)
     }
 }
 
